76aab626344ffae4c00f8e0821cfbb85caff3c8a
Hussein Hany <hussein.hany.cs@gmail.com>
2024-02-02 20:12:07 +0200

    feat(graphql): Use graphQL over http

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./graphql/schema');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
+
 
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}/graphql`);
 });
