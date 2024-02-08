663d36fbe697acc61d0cb2e3298a3be843f3ac16
Hussein Hany <hussein.hany.cs@gmail.com>
2024-02-09 00:54:20 +0200

    Add nutritional-vitamins category to translation.json

--- src/graphql/queries/QueryType.js
+++ src/graphql/queries/QueryType.js
@@ -1,4 +1,9 @@
-const {GraphQLObjectType, GraphQLInt, GraphQLList, GraphQLString} = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLString,
+} = require("graphql");
 const productType = require("../types/ProductType");
 const productImageType = require("../types/ProductImageType");
 const productRatingType = require("../types/ProductRatingType");
@@ -7,139 +12,206 @@ const categoryType = require("../types/CategoryType");
 
 const i18next = require("i18next");
 const { t } = i18next;
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./myDatabase.db');
-
-
+const sqlite3 = require("sqlite3").verbose();
+let db = new sqlite3.Database("./myDatabase.db");
 
 const queryType = new GraphQLObjectType({
-    name: 'Query', fields: {
-        sayHello: {
-            type: GraphQLString, resolve: () => {
-                return t('welcome');
-            }
-        }
-        ,productGetByID: {
-            type: productType, args: {
-                product_id: { type: GraphQLInt },
-                locale: { type: GraphQLString }
-            }, resolve: (source, args, context, info) => {
-                return new Promise((resolve, reject) => {
-                    db.get('SELECT * FROM PRODUCT WHERE product_id = ?', [args.product_id], (err, row) => {
-                        if (err) {
-                            reject(null);
-                        }
-                        
-                        i18next.changeLanguage(args.locale);
-                        if (args.locale === 'ar') {
-                            row.title = i18next.t('1.title');
-                            row.description = i18next.t('1.description');
-                            row.benefits = i18next.t('1.benefits');
-                            row.overview = i18next.t('1.overview');
+  name: "Query",
+  fields: {
+    sayHello: {
+      type: GraphQLString,
+      resolve: () => {
+        return t("welcome");
+      },
+    },
+    productGetByID: {
+      type: productType,
+      args: {
+        product_id: { type: GraphQLInt },
+        locale: { type: GraphQLString },
+      },
+      resolve: (source, args, context, info) => {
+        return new Promise((resolve, reject) => {
+          db.get(
+            "SELECT * FROM PRODUCT WHERE product_id = ?",
+            [args.product_id],
+            (err, row) => {
+              if (err) {
+                reject(null);
+              }
 
-                        }
-                        resolve(row);
-                    });
-                });
+              i18next.changeLanguage(args.locale);
+              if (args.locale === "ar") {
+                row.title = i18next.t(`${args.product_id}.title`);
+                row.description = i18next.t(`${args.product_id}.description`);
+                row.benefits = i18next.t(`${args.product_id}.benefits`);
+                row.overview = i18next.t(`${args.product_id}.overview`);
+              }
+              resolve(row);
             }
-        }, productGetAll: {
-            type: new GraphQLList(productType), resolve: (source, args, context, info) => {
-                return new Promise((resolve, reject) => {
-                    db.all('SELECT * FROM PRODUCT', (err, rows) => {
-                        if (err) {
-                            // console.error(err);
-                            reject([]);
-                        }
-                        resolve(rows);
-                    });
-                });
+          );
+        });
+      },
+    },
+    productGetAll: {
+      type: new GraphQLList(productType),
+      args: { locale: { type: GraphQLString } },
+      resolve: (source, args, context, info) => {
+        return new Promise((resolve, reject) => {
+          db.all("SELECT * FROM PRODUCT", (err, rows) => {
+            if (err) {
+              // console.error(err);
+              reject([]);
             }
-        }, productGetImagesByID: {
-            type: new GraphQLList(productImageType), args: {
-                product_id: {type: GraphQLInt}
-            }, resolve: (source, args, context, info) => {
-                return new Promise((resolve, reject) => {
-                    db.all('SELECT * FROM PRODUCT_IMAGES WHERE product_id = ?', [args.product_id], (err, rows) => {
-                        if (err) {
-                            // console.error(err);
-                            reject([]);
-                        }
-                        resolve(rows);
-                    });
-                });
+
+            if (args.locale === "ar") {
+              rows.forEach((row) => {
+                row.title = i18next.t(`${row.product_id}.title`);
+                row.description = i18next.t(`${row.product_id}.description`);
+                row.benefits = i18next.t(`${row.product_id}.benefits`);
+                row.overview = i18next.t(`${row.product_id}.overview`);
+              });
             }
-        }, productGetRatingsByID: {
-            type: new GraphQLList(productRatingType), args: {
-                product_id: {type: GraphQLInt}
-            }, resolve: (source, args, context, info) => {
-                return new Promise((resolve, reject) => {
-                    db.all('SELECT PRODUCT_RATINGS.*, USER.username FROM PRODUCT_RATINGS INNER JOIN USER ON PRODUCT_RATINGS.user_id = USER.user_id WHERE product_id = ?', [args.product_id], (err, rows) => {
-                        if (err) {
-                            // console.error(err);
-                            reject([]);
-                        }
-                        resolve(rows);
-                    });
-                });
+
+            resolve(rows);
+          });
+        });
+      },
+    },
+    productGetImagesByID: {
+      type: new GraphQLList(productImageType),
+      args: {
+        product_id: { type: GraphQLInt },
+      },
+      resolve: (source, args, context, info) => {
+        return new Promise((resolve, reject) => {
+          db.all(
+            "SELECT * FROM PRODUCT_IMAGES WHERE product_id = ?",
+            [args.product_id],
+            (err, rows) => {
+              if (err) {
+                // console.error(err);
+                reject([]);
+              }
+              resolve(rows);
             }
-        },
-        productGetAllWithImages: {
-            type: new GraphQLList(productType), resolve: (source, args, context, info) => {
-                return new Promise((resolve, reject) => {
-                    db.all('SELECT * FROM PRODUCT', (err, rows) => {
-                        if (err) {
-                            // console.error(err);
-                            reject([]);
-                        }
-                        resolve(rows);
-                    });
-                });
+          );
+        });
+      },
+    },
+    productGetRatingsByID: {
+      type: new GraphQLList(productRatingType),
+      args: {
+        product_id: { type: GraphQLInt },
+      },
+      resolve: (source, args, context, info) => {
+        return new Promise((resolve, reject) => {
+          db.all(
+            "SELECT PRODUCT_RATINGS.*, USER.username FROM PRODUCT_RATINGS INNER JOIN USER ON PRODUCT_RATINGS.user_id = USER.user_id WHERE product_id = ?",
+            [args.product_id],
+            (err, rows) => {
+              if (err) {
+                // console.error(err);
+                reject([]);
+              }
+              resolve(rows);
             }
-        },
-        productAverageRating: {
-            type: GraphQLInt, args: {
-                product_id: {type: GraphQLInt}
-            }, resolve: (source, args, context, info) => {
-                return new Promise((resolve, reject) => {
-                    db.get('SELECT AVG(rating) as average FROM PRODUCT_RATINGS WHERE product_id = ?', [args.product_id], (err, row) => {
-                        if (err) {
-                            // console.error(err);
-                            reject(null);
-                        }
-                        resolve(row.average);
-                    });
-                });
+          );
+        });
+      },
+    },
+    productGetAllWithImages: {
+      type: new GraphQLList(productType),
+      args: { locale: { type: GraphQLString } },
+      resolve: (source, args, context, info) => {
+        return new Promise((resolve, reject) => {
+          db.all("SELECT * FROM PRODUCT", (err, rows) => {
+            if (err) {
+              // console.error(err);
+              reject([]);
             }
-        },
-        userGetByID: {
-            type: userType, args: {
-                user_id: {type: GraphQLInt}
-            }, resolve: (source, args, context, info) => {
-                return new Promise((resolve, reject) => {
-                    db.get('SELECT * FROM USER WHERE user_id = ?', [args.user_id], (err, row) => {
-                        if (err) {
-                            // console.error(err);
-                            reject(null);
-                        }
-                        resolve(row);
-                    });
-                });
+
+            if (args.locale === "ar") {
+              rows.forEach((row) => {
+                row.title = i18next.t(`${row.product_id}.title`);
+                row.description = i18next.t(`${row.product_id}.description`);
+                row.benefits = i18next.t(`${row.product_id}.benefits`);
+                row.overview = i18next.t(`${row.product_id}.overview`);
+              });
             }
-        },
-        categoryGetAll: {
-            type: new GraphQLList(categoryType), resolve: (source, args, context, info) => {
-                return new Promise((resolve, reject) => {
-                    db.all('SELECT * FROM CATEGORIES', (err, rows) => {
-                        if (err) {
-                            // console.error(err);
-                            reject([]);
-                        }
-                        resolve(rows);
-                    });
-                });
+
+            resolve(rows);
+          });
+        });
+      },
+    },
+    productAverageRating: {
+      type: GraphQLInt,
+      args: {
+        product_id: { type: GraphQLInt },
+      },
+      resolve: (source, args, context, info) => {
+        return new Promise((resolve, reject) => {
+          db.get(
+            "SELECT AVG(rating) as average FROM PRODUCT_RATINGS WHERE product_id = ?",
+            [args.product_id],
+            (err, row) => {
+              if (err) {
+                // console.error(err);
+                reject(null);
+              }
+              resolve(row.average);
             }
-        }
-    }
+          );
+        });
+      },
+    },
+    userGetByID: {
+      type: userType,
+      args: {
+        user_id: { type: GraphQLInt },
+      },
+      resolve: (source, args, context, info) => {
+        return new Promise((resolve, reject) => {
+          db.get(
+            "SELECT * FROM USER WHERE user_id = ?",
+            [args.user_id],
+            (err, row) => {
+              if (err) {
+                // console.error(err);
+                reject(null);
+              }
+              resolve(row);
+            }
+          );
+        });
+      },
+    },
+    categoryGetAll: {
+      type: new GraphQLList(categoryType),
+      args: { locale: { type: GraphQLString } },
+      resolve: (source, args, context, info) => {
+        return new Promise((resolve, reject) => {
+          db.all("SELECT * FROM CATEGORIES", (err, rows) => {
+            if (err) {
+              // console.error(err);
+              reject([]);
+            }
+
+            if (args.locale === "ar") {
+              console.log("inside row ar", rows);
+              rows.forEach((row) => {
+                row.title = i18next.t(`categories.${row.slug}.title`);
+              });
+                console.log("inside row ar", rows);
+            }
+
+            resolve(rows);
+          });
+        });
+      },
+    },
+  },
 });
 
 module.exports = queryType;
